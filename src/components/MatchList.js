b3c328648a8b41a16e12a941acd22f419a5db444
Cassie/Ren <cassandra.chen@uoit.net>
2022-01-15 18:52:52 -0500

    add functions to data

--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Match from "./Match";
 import playerData from "../data/playerData";
+import matchData from '../data/matchData';
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
 function MatchList(props){
   return(
-    <section className="MatchList">
+    <section className="PlayerList MatchList">
       <h1>Current Match list</h1>
-      <Match />
+      <Match players={oneMatch, players} winner={oneMatch, winner} scoreDifference={oneMatch, scoreDifference} />
     </section>
   )
 }
